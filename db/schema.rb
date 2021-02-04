4ec6fda819fa1380c0f9fdcc47b97b88e64813af
nzhukovskiy <nzhukovskiy>
2021-02-04 10:06:30 +0300

    Many too many and duties model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_01_091547) do
+ActiveRecord::Schema.define(version: 2021_02_04_063904) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,11 +36,25 @@ ActiveRecord::Schema.define(version: 2021_02_01_091547) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "duties", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.boolean "is_general", default: false
+  end
+
+  create_table "duties_users", id: false, force: :cascade do |t|
+    t.bigint "duty_id"
+    t.bigint "user_id"
+    t.index ["duty_id"], name: "index_duties_users_on_duty_id"
+    t.index ["user_id"], name: "index_duties_users_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "mail"
     t.text "password_digest"
     t.datetime "birth_date"
-    t.boolean "is_admin"
+    t.boolean "is_admin", default: false
     t.integer "rating", default: 0
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
