a816d786d8a72afd6a7047d1fd877995888eba04
nzhukovskiy <nzhukovskiy>
2021-02-06 19:46:46 +0300

    Mailer and login

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
     has_secure_password
 
     validates :mail, uniqueness: true
+    validates :name, presence: true
+    validates :surname, presence: true
+    validates :mail, presence: true
     has_and_belongs_to_many :duties
     def uniq_duties
         duties.distinct
