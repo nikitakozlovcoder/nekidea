9847f4bbbc986fd790dfd875b68774b0d75eb670
nikita <nikita@DESKTOP-EEKA4M8.localdomain>
2021-04-15 12:42:39 +0300

    schedule

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -19,7 +19,7 @@ class Vote < ApplicationRecord
     current_datetime = DateTime.now
     created =  self.created_at.beginning_of_day
     arr.each_with_index do |el, i|
-        created = created.advance(days: el['days_collecting'])
+        created = created.advance(days: el['days_collecting'].to_i)
         unless current_datetime>=created
           new_iter = i+1
           new_status = 0
@@ -28,7 +28,7 @@ class Vote < ApplicationRecord
           break
         end
 
-        created = created.advance(days: el['days_voting'])
+        created = created.advance(days: el['days_voting'].to_i)
         unless current_datetime>=created
           new_iter = i+1
           new_status = 1
@@ -38,7 +38,7 @@ class Vote < ApplicationRecord
     end
     new_status = 2 if archived
 
-    if self.current_iter != new_iter || self.vote_status != new_status
+    if self.current_iter < new_iter || read_attribute_before_type_cast(:vote_status) < new_status
       t =  self.current_iter
       self.current_iter = new_iter
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 User.destroy_all
-
+Schedule.destroy_all
+Schedule.create(checked_at: nil)
 user = User.new(mail: "example@gmail.com", password: "qwerty123", birth_date: 20.years.ago, is_admin: true, rating: 228,
  name: "Omari", surname: "Starks", patronymic: "Ivanovich", restore_date: Time.now.getutc)
 
