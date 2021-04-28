7ac2759151738bd3630e1abc299f6cd02f1858bf
Grandy1 <ystudyone@gmail.com>
2021-04-28 22:43:26 +0300

    Add profile page

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
+  resources :votes
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "home#index"
   get 'login', to: 'users#login'
   post 'login', to: 'users#login_post'
   get 'logout', to: 'users#logout'
-
+  get 'admin', to: 'admin#index'
   get 'idea', to: 'test#idea'
   get 'photos', to: 'test#photos'
+  get 'profile', to: 'test#profile'
+  #get 'vote_create', to: 'test#vote_create'
 
   post 'users/generate', to: 'users#generate'
 
