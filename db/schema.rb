fabeebb45dbd978151a4141cedaa1cc2cc583bb9
nikita <nikita@DESKTOP-EEKA4M8.localdomain>
2021-05-24 10:42:39 +0300

    .

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2160_05_11_164737) do
+ActiveRecord::Schema.define(version: 2160_05_11_164738) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -121,6 +121,7 @@ ActiveRecord::Schema.define(version: 2160_05_11_164737) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "vote_status", default: 0
     t.bigint "duty_id"
+    t.integer "keep_idea_count"
     t.index ["user_id"], name: "index_votes_on_user_id"
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.all.each{|u| u.destroy if u.mail != "example@gmail.com" }
 Schedule.destroy_all
 Schedule.create(checked_at: nil)
 
@@ -15,9 +14,12 @@ d = Duty.where(name: "Все", is_general: true).first_or_create
 Duty.where(name: "Разработчики").first_or_create
 Duty.where(name: "Тестировщики").first_or_create
 Duty.where(name: "Маркетологи").first_or_create
-
-if (u = User.find_by(mail: "example@gmail.com")) == nil
-  user = User.new(mail: "example@gmail.com", password: "qwerty123", birth_date: 20.years.ago, is_admin: false, rating: 228,
+u = User.find_by(mail: "example@gmail.com")
+puts "USER!!!!!!!!!!!!!!"
+pp u
+if (u==nil)
+  puts "CREATING"
+  user = User.new(mail: "example@gmail.com", password: "qwerty123", birth_date: "12.04.1995", is_admin: false, rating: 228,
                   name: "Omari", surname: "Starks", patronymic: "Ivanovich", restore_date: Time.now.getutc, is_boss: true)
 
   user.avatar.attach(io: File.open(File.join(File.dirname(__FILE__), '35.jpg')), filename: '35.jpg')
